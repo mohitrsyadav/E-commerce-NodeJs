022ae2aabe0c68e35f03abab9169a2b070199883
mohitrsyadav <mohitrsyadav@gmail.com>
2024-03-07 15:21:51 +0530

    Sign-in & Sign-up API

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require("express");
-
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/users");
 const app = express()
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, resp)=>{
-    resp.send("App is working");
+// Resistor API==>
+
+app.post("/registor", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
+    console.log("Test", result);
+})
+
+// Login API===>
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user);
+        }
+        else {
+            resp.send({ result: "No result found" });
+        }
+    }
+    else {
+        resp.send({ result: "No result found" });
+    }
 })
 
 app.listen(5000);
