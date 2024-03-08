a331d7d8839c5efec92de61a930d548244ba75eb
mohitrsyadav <mohitrsyadav@gmail.com>
2024-03-08 14:35:52 +0530

    add product & listing product API

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("./db/config");
 const User = require("./db/users");
+const Product = require("./db/product");
 const app = express()
 app.use(express.json());
 app.use(cors());
 
-// Resistor API==>
+// Resistor API===>
 
 app.post("/registor", async (req, resp) => {
     let user = new User(req.body);
@@ -32,6 +33,33 @@ app.post("/login", async (req, resp) => {
     else {
         resp.send({ result: "No result found" });
     }
+})
+
+// Add Product API===>
+
+app.post("/add-product", async (req, resp)=>{
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result);
+    console.log(result);
+})
+
+// Get Product API===>
+
+app.get("/products", async(req, resp)=>{
+    let products =  await Product.find();
+    if(products.length > 0){
+        resp.send(products);
+    }else{
+        resp.send({result: "No Products found"});
+    }
+})
+
+// Delete Product API ===>
+
+app.delete("/product:id", async(req, resp)=>{
+    const result = await Product.deleteOne({_id: req.params.id});
+    resp.send(result);
 })
 
 app.listen(5000);
